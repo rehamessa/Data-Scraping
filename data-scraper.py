4e8683a371569f70aa665f1c515e173b9caa5fc8
reham.essa <rehamessa96@gmail.com>
2026-09-15 00:51:47 +0300

    save the extracted data to text file

--- data-scraper.py
+++ data-scraper.py
@@ -27,6 +27,10 @@ from bs4 import BeautifulSoup
 response=requests.get("https://example.com")
 #print(response.status_code)
 
+if response.status_code==200:
+    print("Website loaded")
+else:
+    print("Error")
 #------------------------------------------------------------
 # Parse the html
 #------------------------------------------------------------
@@ -39,11 +43,15 @@ title=soup.title.text #extract the title
 heading=soup.find('h1').text #extract the main heading
 #print(heading)
 
+paragraph=soup.find('p').text #extract the paragraph
+#print(paragraph)
+
 #------------------------------------------------------------
-# save the dat to text file
+# save the data to text file
 #------------------------------------------------------------
 
 with open("websitedata.txt","w") as file:
     file.write(f"Title:{title}\n\n")
-    file.write(f"Heading:{heading}")
+    file.write(f"Heading:{heading}\n\n")
+    file.write(f"paragraph:{paragraph}")
 
